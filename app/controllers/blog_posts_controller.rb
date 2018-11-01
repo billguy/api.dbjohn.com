44304fe22ef145efa6343c0de1da81d5b1f98549
David John <dj@dbjohn.com>
2018-11-01 16:27:56 -0500

    Added per_page

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,9 +2,9 @@ class BlogPostsController < PostsController
 
   def index
     if current_user
-      @posts = params[:tags].present? ? Post.blogs.tagged_with(params[:tags]).page(params[:page]) : Post.blogs.page(params[:page])
+      @posts = params[:tags].present? ? Post.blogs.tagged_with(params[:tags]).page(params[:page]).per(params[:per_page]) : Post.blogs.page(params[:page]).per(params[:per_page])
     else
-      @posts = params[:tags].present? ? Post.published.blogs.tagged_with(params[:tags]).page(params[:page]) : Post.published.blogs.page(params[:page])
+      @posts = params[:tags].present? ? Post.published.blogs.tagged_with(params[:tags]).page(params[:page]).per(params[:per_page]) : Post.published.blogs.page(params[:page]).per(params[:per_page])
     end
     render json: @posts, meta: { page: params[:page].to_i, total_pages: @posts.total_pages, per_page: Post.default_per_page, tag_list: Post.tag_counts_on(:tags) }, root: root
   end
