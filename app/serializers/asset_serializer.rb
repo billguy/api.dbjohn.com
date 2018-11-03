1157eda355b752912fa2693e6aa8061f91deba06
David John <dj@dbjohn.com>
2018-11-03 13:06:05 -0500

    Refactor AssetSerializer

--- app/serializers/asset_serializer.rb
+++ app/serializers/asset_serializer.rb
@@ -5,9 +5,7 @@ class AssetSerializer < ActiveModel::Serializer
   attributes :id, :url, :thumb, :link, :attachment
 
   def url
-    if object.attachment.attached?
-      Rails.env.production? ? object.attachment.service_url : rails_blob_url(object.attachment)
-    end
+    rails_blob_url(object.attachment) if object.attachment.attached?
   end
 
   def link
@@ -16,11 +14,7 @@ class AssetSerializer < ActiveModel::Serializer
 
   def thumb
     if object.attachment.attached?
-      if Rails.env.production?
-        object.attachment.variant(resize: "200x133").processed.service_url
-      else
-        rails_representation_url(object.attachment.variant(resize: "200x133").processed)
-      end
+      rails_representation_url(object.attachment.variant(resize: "200x133").processed)
     end
   end
 
