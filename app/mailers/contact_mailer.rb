97561170ce502ceff77afba959e983d42db7093a
David John <dj@dbjohn.com>
2024-10-13 14:40:04 -0500

    update mailer cfg

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ActionMailer::Base
 
   def new_contact(contact)
     @contact = contact
-    mail(to: ENV['admin_email'], from: "#{@contact.name} <#{@contact.email}>", subject: 'DBJohn.com contact form submission')
+    mail(to: ENV['admin_email'], reply_to: "#{@contact.name} <#{@contact.email}>", subject: 'DBJohn.com contact form submission')
   end
 end
